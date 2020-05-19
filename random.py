4cfba028b6c51ab8d4bebb65ec358630a2a08c6f
tanv321 <65573002+tanv321@users.noreply.github.com>
2020-05-18 22:47:46 -0400

    this fixes #3

--- random.py
+++ random.py
@@ -3,4 +3,4 @@ a = random.randint(1,11)
 print("random number generator: {}".format(a))
 for i in range(5,10):
     print(random.randint(1,10))
-#updated the wrong #3a to #3
+#updated the wrong #3a to #3 this will fix the chain
